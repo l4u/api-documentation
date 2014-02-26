72c0b38a204d0be7275ba44b02b591569527c9d3
Tristan Dunn <tristanzdunn@gmail.com>
2014-02-26 15:04:03 -0600

    Add user shots documentation.

---
+++ content/v1/users/shots.md
@@ -0,0 +1,23 @@
+---
+title: Users - Shots | Dribbble API
+---
+
+# Shots
+
+* TOC
+{:toc}
+
+## List shots for a user
+
+List a user's shots:
+
+    GET /users/:id/shots
+
+List the authenticated user's shots:
+
+    GET /user/shots
+
+### Response
+
+<%= headers 200 %>
+<%= json(:shot_without_user) { |hash| [hash] } %>
--- lib/resources.rb
+++ lib/resources.rb
@@ -66,7 +66,7 @@ module Dribbble
         updated_at: "2013-10-13T21:48:27Z"
       }
 
-      SHOT = {
+      SHOT_WITHOUT_USER = {
         id: 1,
         title: "Working on the new shop",
         animated: false,
@@ -76,9 +76,12 @@ module Dribbble
         attachments: 0,
         rebounds: 0,
         created_at: "2009-07-08T11:08:35Z",
-        updated_at: "2011-06-25T16:51:39Z",
-        user: USER
+        updated_at: "2011-06-25T16:51:39Z"
       }
+
+      SHOT = SHOT_WITHOUT_USER.merge(
+        user: USER
+      )
 
       def headers(status, headers = {})
         class_name = "headers"
